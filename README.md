62a681dd31f5dac6fa87881c5aabcd10dcf2eba7
rhaas <rhaas@intermade.be>
2023-02-14 17:55:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Test publish on npm site
---
+++ tests/common.test.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react'
+import { render } from '@testing-library/react'
+
+import 'jest-canvas-mock'
+
+import { MyCounter } from '../src'
+
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<MyCounter />)
+  })
+})
